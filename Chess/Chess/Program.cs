09e39b41f18e88e69ce100c67d12a2086320611a
Brandon Frederick <1ultimatedj@gmail.com>
2017-09-17 11:16:39 -0700

    initial commit

---
+++ Chess/Chess/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                                             ");
+            Console.WriteLine("      A    B    C    D    E    F    G    H   ");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  1 | WR | WN | WB | WQ | WK | WB | WN | WR |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  2 | WP | WP | WP | WP | WP | WP | WP | WP |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  3 |    |    |    |    |    |    |    |    |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  4 |    |    |    |    |    |    |    |    |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  5 |    |    |    |    |    |    |    |    |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  6 |    |    |    |    |    |    |    |    |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  7 | BP | BP | BP | BP | BP | BP | BP | BP |");
+            Console.WriteLine("    -----------------------------------------");
+            Console.WriteLine("  8 | BR | BN | BB | BQ | BK | BB | BN | BR |");
+            Console.WriteLine("    -----------------------------------------");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
